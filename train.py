97085f74f14fc31da70d3991148c6fb4e4abf05c
hyunwoo3235 <hyunwoo3235@gmail.com>
2021-02-18 01:01:56 +0900

    migration to pytorch

--- train.py
+++ train.py
@@ -1,113 +1,129 @@
-import time
-import tensorflow as tf
-import numpy as np
+import argparse
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import MultiStepLR
 
-from models.SRGAN import RRDBNet
-from models.discriminator import Discriminator_VGG_128, VGGFeatureExtractor
-
-
-from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow.keras.losses import MeanSquaredError
-from tensorflow.keras.metrics import Mean
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
-
-
-def cri_gan(x, target_is_real):
-    if target_is_real:
-        target_label = tf.fill(x.shape, 1.0)
-    else:
-        target_label = tf.fill(x.shape, 0.0)
-    loss = tf.nn.sigmoid_cross_entropy_with_logits(x, target_label)
-    return loss
-
-
-class SrganTrainer:
-    def __init__(self,
-                 generator,
-                 discriminator,
-                 learning_rate=PiecewiseConstantDecay(boundaries=[100000], values=[1e-4, 1e-5])):
-
-        self.generator = generator
-        self.discriminator = discriminator
-        self.netF = VGGFeatureExtractor(feature_layer=34)
-
-        self.generator_optimizer = Adam(learning_rate=learning_rate)
-        self.discriminator_optimizer = Adam(learning_rate=learning_rate)
-
-        self.binary_cross_entropy = BinaryCrossentropy(from_logits=False)
-        self.mean_squared_error = MeanSquaredError()
-
-    def train(self, train_dataset, steps=200):
-        pls_metric = Mean()
-        dls_metric = Mean()
-        step = 0
-
-        for lr, hr in train_dataset.take(steps):
-            step += 1
-
-            pl, dl = self.train_step(lr, hr)
-            pls_metric(pl)
-            dls_metric(dl)
-
-            if step % 10 == 0:
-                print(
-                    f'{step}/{steps}, perceptual loss = {pls_metric.result():.4f},'
-                    f' discriminator loss = {dls_metric.result():.4f}'
-                )
-                pls_metric.reset_states()
-                dls_metric.reset_states()
-
-    @tf.function
-    def train_step(self, lr, hr):
-        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            lr = tf.cast(lr, tf.float32)
-            hr = tf.cast(hr, tf.float32)
-
-            sr = self.generator(lr)
-
-            l_g_total = 0
-
-            l_g_pix = 1e-2 * tf.reduce_mean(tf.abs(sr, hr))
-            l_g_total += l_g_pix
-
-            real_fea = self.netF(hr)
-            fake_fea = self.netF(sr)
-            l_g_fea = tf.reduce_mean(tf.abs(real_fea - fake_fea))
-            l_g_total += l_g_fea
-
-            pred_g_fake = self.discriminator(sr)
-            pred_d_real = self.discriminator(hr)
-            l_g_gan = 5e-3 * (
-                cri_gan(pred_d_real - pred_g_fake, False) +
-                cri_gan(pred_g_fake - pred_d_real, True)) / 2
-            l_g_total += l_g_gan
-
-            pred_d_real = self.discriminator(hr)
-            pred_d_fake = self.discriminator(sr)
-            l_d_real = cri_gan(pred_d_real - pred_d_fake, True)
-            l_d_fake = cri_gan(pred_d_fake - pred_d_real, False)
-            l_d_total = (l_d_real + l_d_fake) / 2
-
-        gradients_of_generator = gen_tape.gradient(l_g_total, self.generator.trainable_variables)
-        gradients_of_discriminator = disc_tape.gradient(l_d_total, self.discriminator.trainable_variables)
-
-        self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
-        self.discriminator_optimizer.apply_gradients(
-            zip(gradients_of_discriminator, self.discriminator.trainable_variables))
-
-        return l_g_total, l_d_total
+from data.dataset import ImageDataset
+from models.loss import GANLoss
+import models.discriminator_vgg_arch as SRGAN_arch
+import models.RRDBNet_arch as RRDBNet_arch
 
 
 def main():
-    generator = RRDBNet(nf=64, nb=23)
-    discriminator = Discriminator_VGG_128()
-    trainer = SrganTrainer(
-        generator=generator,
-        discriminator=discriminator
-    )
-    trainer.train(None, 12)
-    return 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-opt', type=str)
+    parser.add_argument('-load', type=str, default='')
+    args = parser.parse_args()
+
+    writer = SummaryWriter(log_dir='../tensorboard')
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    netG = RRDBNet_arch.RRDBNet(in_nc=3, out_nc=3,
+                                nf=64, nb=13).to(device)
+    netD = SRGAN_arch.NLayerDiscriminator(input_nc=3, ndf=3, n_layers=3).to(device)
+    netF = SRGAN_arch.VGGFeatureExtractor(feature_layer=34, use_bn=False,
+                                          use_input_norm=True, device=device)
+    netF.eval()
+    netG.train()
+    netD.train()
+
+    optimizers = []
+    schedulers = []
+
+    cri_pix = nn.L1Loss().to(device)
+    cri_fea = nn.L1Loss().to(device)
+    cri_gan = GANLoss('ragan').to(device)
+
+    optim_params = []
+    for k, v in netG.named_parameters():
+        if v.requires_grad:
+            optim_params.append(v)
+
+    optimizer_G = torch.optim.Adam(optim_params,
+                                   lr=1e-4,
+                                   weight_decay=0,
+                                   betas=(0.9, 0.999))
+    optimizer_D = torch.optim.Adam(netD.parameters(),
+                                   lr=1e-4,
+                                   weight_decay=0,
+                                   betas=(0.9, 0.999))
+
+    optimizers.append(optimizer_G)
+    optimizers.append(optimizer_D)
+
+    for optimizer in optimizers:
+        schedulers.append(MultiStepLR(optimizer,
+                                      milestones=[5000, 10000, 20000, 30000],
+                                      gamma=0.5))
+
+    train_set = ImageDataset('dataset_opt')
+    train_loader = DataLoader(train_set, batch_size=16, shuffle=True)
+
+    current_step = 0
+
+    for _ in range(50):
+        for _, train_data in enumerate(train_loader):
+            current_step += 1
+
+            var_L = train_data['LQ'].to(device)
+            var_H = train_data['GT'].to(device)
+            var_ref = var_H
+
+            for p in netD.parameters():
+                p.requires_grad = False
+
+            optimizer_G.zero_grad()
+            fake_H = netG(var_L.detach())
+
+            l_g_total = 0
+
+            l_g_pix = 1e-2 * cri_pix(fake_H, var_H)
+            l_g_total += l_g_pix
+
+            real_fea = netF(var_H).detach()
+            fake_fea = netF(fake_H)
+            l_g_fea = cri_fea(fake_fea, real_fea)
+            l_g_total += l_g_fea
+
+            pred_g_fake = netD(fake_H)
+            pred_d_real = netD(var_ref).detach()
+            l_g_gan = 5e-3 * (
+                    cri_gan(pred_d_real - torch.mean(pred_g_fake), False) +
+                    cri_gan(pred_g_fake - torch.mean(pred_d_real), True)) / 2
+            l_g_total += l_g_gan
+
+            l_g_total.backward()
+            optimizer_G.step()
+
+            # 이제 D
+            for p in netD.parameters():
+                p.requires_grad = True
+
+            optimizer_D.zero_grad()
+            pred_d_real = netD(var_ref)
+            pred_d_fake = netD(fake_H.detach())  # detach to avoid BP to G
+            l_d_real = cri_gan(pred_d_real - torch.mean(pred_d_fake), True)
+            l_d_fake = cri_gan(pred_d_fake - torch.mean(pred_d_real), False)
+            l_d_total = (l_d_real + l_d_fake) / 2
+
+            l_d_total.backward()
+            optimizer_D.step()
+
+            for scheduler in schedulers:
+                scheduler.step()
+
+            if current_step % 100 == 0:
+                writer.add_scalar('l_g_pix', l_g_pix.item())
+                writer.add_scalar('l_g_fea', l_g_fea.item())
+                writer.add_scalar('l_g_gan', l_g_gan.item())
+                writer.add_scalar('l_d_real', l_d_real.item())
+                writer.add_scalar('l_d_fake', l_d_fake.item())
+
+                writer.add_scalar('D_real', torch.mean(pred_d_real.detach()))
+                writer.add_scalar('D_fake', torch.mean(pred_d_fake.detach()))
 
 
 if __name__ == '__main__':
